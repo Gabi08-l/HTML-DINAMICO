f127e684d3040066a1fb725636092618c60397c4
Gabi08-l <senaioloko@gmail.com>
2025-03-21 09:22:10 -0300

    exercicio 2 corrigido

--- Atividade01/js/script.js
+++ Atividade01/js/script.js
@@ -1,26 +1,45 @@
 const trocarImagem = () => {
-let imagem = document.getElementById("imagem")
+  let imagem = document.getElementById("imagem");
 
-const trocarImagens = [
-    "img/homemara.jpg",
-    "img/davi.jpg",
-    "img/britto.jpg"
-]
+  const trocarImagens = ["img/homemara.jpg", "img/davi.jpg", "img/britto.jpg"];
 
+  let indice = 0;
+  let numero = Math.random();
 
-let indice = 0 
-let numero = Math.random()  
+  if (numero > 0.75) indice = 3;
+  else if (numero > 0.5) indice = 2;
+  else if (numero > 0.25) indice = 1;
+  else indice = 0;
 
-if (numero >0.75) indice = 3;
-else if (numero > 0.5) indice = 2;
-else if (numero > 0.25) indice = 1;
-else indice = 0;
-
-
-    
-imagem.src= trocarImagens[indice]
+  imagem.src = trocarImagens[indice];
 };
 
 
 
+const calcular = (operador) => {
+  let valor1 = parseFloat (frmCalculadora.inValor1.value);
+  let valor2 = parseFloat (frmCalculadora.inValor2.value);
+  let resultado = 0;
+
+switch (operador) {
+    case "+":
+        resultado = valor1 + valor2
+        
+        break;
+        
+        case "-":
+            resultado = valor1 - valor2
+            break;
+        
+            case "*":
+        resultado = valor1 * valor2
+            break;
+
+            case "/":
+        resultado = valor1 / valor2
+        break;
+}
+
+  frmCalculadora.inResultado.value = resultado
+};
 
